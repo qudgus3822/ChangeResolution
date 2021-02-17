1226fce51c7d7ea0693b47df7a466b9afbcf7bdb
bhkim <bhkim@stis.co.kr>
2021-02-17 10:53:26 +0900

    초기 커밋.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.Write(Screen.PrimaryScreen);
+
+
+            Screen s = Screen.PrimaryScreen;
+
+            Console.Write("test");
+            //ChangeDisplaySettings(1920, 1080);
+        }
+        public static void GetCurrentSettings()
+        {
+            DEVMODE mode = new DEVMODE();
+            mode.dmSize = (ushort)Marshal.SizeOf(mode);
+
+            if (EnumDisplaySettings(null,
+                -1,
+                ref mode) == true) // Succeeded
+            {
+                Console.WriteLine("Current Mode:\n\t" +
+                    "{0} by {1}, " +
+                    "{2} bit, " +
+                    "{3} degrees, " +
+                    "{4} hertz",
+                    mode.dmPelsWidth,
+                    mode.dmPelsHeight,
+                    mode.dmBitsPerPel,
+                    mode.dmDisplayOrientation * 90,
+                    mode.dmDisplayFrequency);
+            }
+        }
+
+        public static void ChangeDisplaySettings
+    (int width, int height)
+        {
+            DEVMODE originalMode = new DEVMODE();
+            originalMode.dmSize =
+                (ushort)Marshal.SizeOf(originalMode);
+
+            // Retrieving current settings
+            // to edit them
+            EnumDisplaySettings(null,
+                -1,
+                ref originalMode);
+
+            // Making a copy of the current settings
+            // to allow reseting to the original mode
+            DEVMODE newMode = originalMode;
+
+            // Changing the settings
+            newMode.dmPelsWidth = (uint)width;
+            newMode.dmPelsHeight = (uint)height;
+
+            // Capturing the operation result
+            int result =
+                ChangeDisplaySettings(ref newMode, 0);
+
+            if (result == 0)
+            {
+                Console.WriteLine("Succeeded.\n");
+
+                // Inspecting the new mode
+                GetCurrentSettings();
+
+                Console.WriteLine();
+
+                ChangeDisplaySettings(ref originalMode, 0);
+            }
+            else if (result == -2)
+                Console.WriteLine("Mode not supported.");
+            else if (result == 1)
+                Console.WriteLine("Restart required.");
+            else
+                Console.WriteLine("Failed. Error code = {0}", result);
+        }
+
+        [DllImport("User32.dll")]
+        [return: MarshalAs(UnmanagedType.I4)]
+        public static extern int ChangeDisplaySettings(
+    [In, Out]
+    ref DEVMODE lpDevMode,
+    [param: MarshalAs(UnmanagedType.U4)]
+    uint dwflags);
+
+        [DllImport("User32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern Boolean EnumDisplaySettings(
+    [param: MarshalAs(UnmanagedType.LPTStr)]
+    string lpszDeviceName,
+    [param: MarshalAs(UnmanagedType.U4)]
+    int iModeNum,
+    [In, Out]
+    ref DEVMODE lpDevMode);
+    }
+
+    [StructLayout(LayoutKind.Sequential,
+      CharSet = CharSet.Ansi)]
+    public struct DEVMODE
+    {
+        // You can define the following constant
+        // but OUTSIDE the structure because you know
+        // that size and layout of the structure
+        // is very important
+        // CCHDEVICENAME = 32 = 0x50
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string dmDeviceName;
+        // In addition you can define the last character array
+        // as following:
+        //[MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+        //public Char[] dmDeviceName;
+
+        // After the 32-bytes array
+        [MarshalAs(UnmanagedType.U2)]
+        public UInt16 dmSpecVersion;
+
+        [MarshalAs(UnmanagedType.U2)]
+        public UInt16 dmDriverVersion;
+
+        [MarshalAs(UnmanagedType.U2)]
+        public UInt16 dmSize;
+
+        [MarshalAs(UnmanagedType.U2)]
+        public UInt16 dmDriverExtra;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmFields;
+
+        public POINTL dmPosition;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmDisplayOrientation;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmDisplayFixedOutput;
+
+        [MarshalAs(UnmanagedType.I2)]
+        public Int16 dmColor;
+
+        [MarshalAs(UnmanagedType.I2)]
+        public Int16 dmDuplex;
+
+        [MarshalAs(UnmanagedType.I2)]
+        public Int16 dmYResolution;
+
+        [MarshalAs(UnmanagedType.I2)]
+        public Int16 dmTTOption;
+
+        [MarshalAs(UnmanagedType.I2)]
+        public Int16 dmCollate;
+
+        // CCHDEVICENAME = 32 = 0x50
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string dmFormName;
+        // Also can be defined as
+        //[MarshalAs(UnmanagedType.ByValArray,
+        //    SizeConst = 32, ArraySubType = UnmanagedType.U1)]
+        //public Byte[] dmFormName;
+
+        [MarshalAs(UnmanagedType.U2)]
+        public UInt16 dmLogPixels;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmBitsPerPel;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmPelsWidth;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmPelsHeight;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmDisplayFlags;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmDisplayFrequency;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmICMMethod;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmICMIntent;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmMediaType;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmDitherType;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmReserved1;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmReserved2;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmPanningWidth;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public UInt32 dmPanningHeight;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINTL
+    {
+        [MarshalAs(UnmanagedType.I4)]
+        public int x;
+        [MarshalAs(UnmanagedType.I4)]
+        public int y;
+    }
+
+}
+
+
